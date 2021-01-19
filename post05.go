70a346daa9996d1011a592ecbe75b0a6be93ee20
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2021-01-19 20:08:59 +0200

    Basic package structure

---
+++ post05.go
@@ -0,0 +1,26 @@
+package post05
+
+type User struct {
+}
+
+type Userdata struct {
+}
+
+func AddUSer() bool {
+
+	return true
+}
+
+func DeleteUser(id int) bool {
+
+	return true
+}
+
+func ListUsers() {
+
+}
+
+func UpdateUser() bool {
+
+	return true
+}
