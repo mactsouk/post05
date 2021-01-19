0451a9dab876d636f066b1b7868ddc15fcd30a23
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2021-01-19 20:12:52 +0200

    Structures -> Tables

--- post05.go
+++ post05.go
@@ -1,12 +1,24 @@
 package post05
 
 type User struct {
+	ID       int
+	Username string
 }
 
 type Userdata struct {
+	ID          int
+	Name        string
+	Surname     string
+	Description string
 }
 
-func AddUSer() bool {
+// The function returns the User ID of the user
+// -1 if the user does not exist
+func UsernameExists(id int) int {
+
+}
+
+func AddUser() bool {
 
 	return true
 }
